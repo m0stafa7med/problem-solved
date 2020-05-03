db13a533077fa02580056cc59fbc95d6bffdbc07
m0stafa7med <45430770+m0stafa7med@users.noreply.github.com>
2020-05-03 02:36:35 +0200

    Add files via upload

---
+++ Maximum Subarray/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int maxSubArr (int arr[],int n)
+{
+    int maxSum =arr[0],maxCurrent=arr[0];
+    for(int i=0; i<n; i++)
+    {
+        maxCurrent=max(maxCurrent+arr[i],arr[i]);
+        maxSum=max(maxSum,maxCurrent);
+    }
+    return maxSum;
+}
+int main()
+{
+    cout << "Enter array's size :";
+    int arrSize;
+    cin >> arrSize;
+    int *array=new int [arrSize];
+    for(int i=0; i<arrSize; i++)
+        cin >> array[i];
+    cout << "the max sum :  " << maxSubArr(array,arrSize) << "\n";
+
+    return 0;
+}
