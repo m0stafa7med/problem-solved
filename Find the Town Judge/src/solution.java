142dd97c88c55b58159352af2de72cdb6ec0508e
m0stafa7med <45430770+m0stafa7med@users.noreply.github.com>
2020-05-11 21:18:39 +0200

    Add files via upload

---
+++ Find the Town Judge/src/solution.java
@@ -0,0 +1,23 @@
+public class solution {
+    public static int findJudge(int N, int[][] trust) {
+        int [] count = new int [N+1];
+        for(int [] t : trust)
+        {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for(int i=1;i<=N;i++)
+            if(count[i]==N-1) return i;
+        return -1;
+
+        
+    }
+    
+    public static void main(String[] args) {
+    	
+    	int [][]arr={{1,3},{1,4},{2,3},{2,4},{4,3}};
+    System.out.println(findJudge(4,arr));
+    }
+    
+    
+}
